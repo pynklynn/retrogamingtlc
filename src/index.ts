d0549739f538662e93290a9daf1f8a3a8e056c9a
Will Steinmetz <willsteinmetz86+git@gmail.com>
2021-10-17 17:26:16 -0400

    chore: moved site nav code into component

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter';
 import '@webcomponents/webcomponentsjs/webcomponents-bundle';
 
 import './styles/app.scss';
+import './components/site-nav.component';
 import './components/nav-item.component';
