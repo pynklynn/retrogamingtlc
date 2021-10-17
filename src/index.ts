54ae5b1ae20350d150460edc312c8b6643e88186
Will Steinmetz <willsteinmetz86+git@gmail.com>
2021-10-17 16:58:50 -0400

    chore: Removing IG code

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,3 @@ import '@webcomponents/webcomponentsjs/webcomponents-bundle';
 
 import './styles/app.scss';
 import './components/nav-item.component';
-import './components/ig-feed.component';
-import './components/ig-post.component';
--- src/services/instagram-rss.service.ts
+++
@@ -1,9 +0,0 @@
-import { parse } from 'rss-to-json';
-
-async function fetchInstagramFeed() {
-  return await parse('https://rss.app/feeds/RK2cK11CyHaLxPjm.xml', {});
-}
-
-export {
-  fetchInstagramFeed
-};
