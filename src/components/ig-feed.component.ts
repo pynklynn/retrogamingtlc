b95956037b9ed2e4d5a5d9b5d65e7c483b5f41e0
Will Steinmetz <willsteinmetz86+git@gmail.com>
2021-10-17 16:54:44 -0400

    chore: Adding more IG code

--- src/components/ig-feed.component.ts
+++ src/components/ig-feed.component.ts
@@ -13,25 +13,21 @@ export class RtgIgFeed extends Slotify(LitElement) {
 
   public async firstUpdated(): Promise<void> {
     this.template = (this.querySelector('template') as HTMLTemplateElement).innerHTML.trim();
-    console.log(this.template);
-    // try {
-      console.log(await fetchInstagramFeed());
+    try {
       this.instagramFeed = (await fetchInstagramFeed()).items.reverse();
-      const parser = new DOMParser();
-      for (let i = 0, len = this.instagramFeed.length; i < len; i++) {
-        const descriptionDoc = parser.parseFromString(this.instagramFeed[i].description, 'text/html');
-        const imageUrl = (descriptionDoc.querySelector('img') as HTMLImageElement).src;
-        const descriptionText = (descriptionDoc.querySelector('div div') as HTMLElement).innerText;
-        const elementTemplate = `${this.template}`.replace('{{image}}', imageUrl).replace('{{description}}', descriptionText);
-        console.log(elementTemplate);
-        // const post = parser.parseFromString(elementTemplate, 'text/xml');
-        // console.log(post);
-        // this.appendChild(post);
-        this.insertAdjacentHTML('afterend', elementTemplate);
-      }
-    // } catch (e) {
-    //   // do nothing
-    // }
+    } catch (e) {
+      // do nothing
+    }
+    const parser = new DOMParser();
+    for (let i = 0, len = this.instagramFeed.length; i < len; i++) {
+      const descriptionDoc = parser.parseFromString(this.instagramFeed[i].description, 'text/html');
+      console.log(descriptionDoc);
+      const imageUrl = (descriptionDoc.querySelector('img') as HTMLImageElement).src;
+      const descriptionText = (descriptionDoc.querySelector('div div') as HTMLElement).innerText;
+      const elementTemplate = `${this.template}`.replace('{{image}}', imageUrl).replace('{{description}}', descriptionText);
+      console.log(elementTemplate);
+      this.insertAdjacentHTML('afterend', elementTemplate);
+    }
   }
 
   public render(): TemplateResult {
