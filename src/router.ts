738cefffb2e5eb8f91d3555942f08f0638179274
Will Steinmetz <willsteinmetz86+git@gmail.com>
2022-05-12 01:31:49 -0400

    chore(routing): setting up routing for GH pages

--- src/router.ts
+++ src/router.ts
@@ -3,6 +3,15 @@ import { Router } from '@vaadin/router';
 const outlet = document.querySelector('main');
 const router = new Router(outlet);
 router.setRoutes([
-  { path: '/', component: 'rtg-home-view' },
-  { path: '/restoration', component: 'rtg-restoration-view' }
+  { path: '/', component: 'rtg-home-view', name: 'home' },
+  { path: '/restoration', component: 'rtg-restoration-view', name: 'restoration' }
 ]);
+
+// handle routing for GitHub pages
+const path = localStorage.getItem('path');
+if (path) {
+  localStorage.removeItem('path');
+  Router.go({
+    pathname: `/${path}`
+  });
+}
